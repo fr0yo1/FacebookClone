c0c207bff2b52c8da9a3c0f2fe2fa6ab21414371
Adrian <adrian.sandru36@yahoo.com>
2018-12-16 18:44:20 +0200

    Fixed misspelled vars.

--- FacebookClone/FacebookClone/Models/MessengerViewModel.cs
+++ FacebookClone/FacebookClone/Models/MessengerViewModel.cs
@@ -8,13 +8,15 @@ namespace FacebookClone.Models
     public class MessengerViewModel
     {
         public List<AspNetUser> friends { get; set; }
-        public List<Message> requests { get; set; }
+        public List<Message> receivedRequests { get; set; }
+        public List<Message> sentRequests { get; set; }
         public ConversationViewModel conversation { get; set; }
 
         public MessengerViewModel(AspNetUser user)
         {
             friends = user.AspNetUsers.ToList();
-            requests = user.Messages.Where(x=> x.type == Convert.ToInt32(MessageTypes.friendRequest) || x.type == Convert.ToInt32(MessageTypes.groupRequest)).ToList();
+            receivedRequests = user.Messages.Where(x=> x.type == Convert.ToInt32(MessageTypes.friendRequest) || x.type == Convert.ToInt32(MessageTypes.groupRequest)).ToList();
+            sentRequests = user.Messages1.Where(x => x.type == Convert.ToInt32(MessageTypes.friendRequest) || x.type == Convert.ToInt32(MessageTypes.groupRequest)).ToList();
         }
 
         public MessengerViewModel(string receiver_id, string sender_id)
@@ -22,7 +24,7 @@ namespace FacebookClone.Models
             FacebookDatabaseEntities databaseEntities = new FacebookDatabaseEntities();
             var user = databaseEntities.AspNetUsers.Find(receiver_id);
             friends = user.AspNetUsers.ToList();
-            requests = user.Messages.Where(x => x.type == Convert.ToInt32(MessageTypes.friendRequest) || x.type == Convert.ToInt32(MessageTypes.groupRequest)).ToList();
+            receivedRequests = user.Messages.Where(x => x.type == Convert.ToInt32(MessageTypes.friendRequest) || x.type == Convert.ToInt32(MessageTypes.groupRequest)).ToList();
 
             var messages = user.Messages.Where(x => x.sender_id == sender_id && x.receiver_id == receiver_id).ToList();
             conversation = new ConversationViewModel(messages);
